949c31bae670775ec980e6b33d64150228aa7b40
Ravi Yadav <shortanswer12345@gmail.com>
2024-03-28 11:10:26 +0530

    adding new origin

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -23,8 +23,8 @@ export async function POST(req,res) {
           },
         ],
         mode: "payment",
-        cancel_url: `${host}`,
-        success_url: `${host}/home`,
+        cancel_url: `${window.location.origin}`,
+        success_url: `${window.location.origin}/home`,
       });
     return  NextResponse.json({ sessionId: session.id },{status:200})
     } catch (err) {
