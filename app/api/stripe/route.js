ec677da5a4a9ac50598a9503726710a3031e7a99
Ravi Yadav <shortanswer12345@gmail.com>
2024-03-28 14:02:15 +0530

    adding new url

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -23,8 +23,8 @@ export async function POST(req,res) {
           },
         ],
         mode: "payment",
-        cancel_url: `${host}`,
-        success_url: `${host}/home`,
+        cancel_url: `https://scriptly-ten.vercel.app/`,
+        success_url: `https://scriptly-ten.vercel.app/home`,
       });
     return  NextResponse.json({ sessionId: session.id },{status:200})
     } catch (err) {
